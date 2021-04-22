89fd330764ba27f6862c8be3fe4e01183db45798
Ian Hecker <ianhecker@yahoo.com>
2021-04-22 17:12:22 -0600

    [Chore] Add api documenation
    Co-authored-by: Ian Hecker <ian@blocky.rocks>

--- prettyprinter.go
+++ prettyprinter.go
@@ -2,6 +2,8 @@ package prettyprinter
 
 import "os"
 
+// PrettyPrinter holds data for printing, interfaces for writing,
+// and the internal error that may occur from printing attempts
 type PrettyPrinter struct {
 	spool  interface{}
 	stderr Writer
@@ -9,16 +11,19 @@ type PrettyPrinter struct {
 	err    error
 }
 
+// Returns a new PrettyPrinter with default stdout and stderr files
 func NewPrettyPrinter() *PrettyPrinter {
 	return NewPrettyPrinterFromRaw(os.Stderr, os.Stdout)
 }
 
+// Construct a PrettyPrinter from raw Writers
 func NewPrettyPrinterFromRaw(
 	stderr, stdout Writer,
 ) *PrettyPrinter {
 	return &PrettyPrinter{nil, stderr, stdout, nil}
 }
 
+// Add any one thing to the PrettyPrinter to store internally for printing
 func (p *PrettyPrinter) Add(
 	value interface{},
 ) Printer {
@@ -26,14 +31,17 @@ func (p *PrettyPrinter) Add(
 	return p
 }
 
+// Pretty print the spool to the stderr Writer
 func (p *PrettyPrinter) StderrDump() Printer {
 	return p.Dump(p.stderr)
 }
 
+// Pretty print the spool to the stdout Writer
 func (p *PrettyPrinter) StdoutDump() Printer {
 	return p.Dump(p.stdout)
 }
 
+// Pretty print the spool to a provided Writer, such as os.File
 func (p *PrettyPrinter) Dump(w Writer) Printer {
 	bytes, err := prettyJSON(p.spool)
 	p.Flush()
@@ -45,14 +53,18 @@ func (p *PrettyPrinter) Dump(w Writer) Printer {
 	return p
 }
 
+// Clear the spool of anything saved for printing
 func (p *PrettyPrinter) Flush() {
 	p.spool = nil
 }
 
+// Return the internal error which may result from a printing attempt
 func (p *PrettyPrinter) Error() error {
 	return p.err
 }
 
+// If there is an internal error from printing, clean the spool and attempt
+// to print the internal error in pretty json to the stderr Writer
 func (p *PrettyPrinter) StderrDumpOnError() error {
 	if p.Error() != nil {
 		p.Flush()
